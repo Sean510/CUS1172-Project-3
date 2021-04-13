43b3f705c5c78ae663300373f7453b46f5a78e98
Sean510 <swhite51096@gmail.com>
2021-04-13 16:22:44 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  
+  document.querySelector("#page_view").onclick = (e) => {
+    
+    handle_app_page_event(e);
+    
+  };
+  
+});
 
+function handle_app_page_event(e) {
+  
+  if (e.target.id == "quiz1") {
+  
+    alert("quiz 1 pressed");
+    //display_quiz("#first_quiz");
+    
+  } else if (e.target.id == "quiz2") {
+  
+    alert("quiz 2 pressed")
+    //display_quiz("#second_quiz");
+  }
+  
+};
