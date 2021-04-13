740e589232a6c25b8113a164e1892847cd60e932
Sean510 <swhite51096@gmail.com>
2021-04-13 16:32:11 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,6 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function handle_app_page_event(e) {
   
+  console.log(e.target.id);
+  
   if (e.target.id == "quiz1") {
   
     alert("quiz 1 pressed");
