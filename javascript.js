6ff605da392ebcebce8ebe8e09e814bd7f4b286b
Sean510 <swhite51096@gmail.com>
2021-04-13 16:35:39 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -12,12 +12,12 @@ function handle_app_page_event(e) {
   
   console.log(e.target.value);
   
-  if (e.target.id == "quiz1") {
+  if (e.target.value == "quiz1") {
   
     alert("quiz 1 pressed");
     //display_quiz("#first_quiz");
     
-  } else if (e.target.id == "quiz2") {
+  } else if (e.target.value == "quiz2") {
   
     alert("quiz 2 pressed")
     //display_quiz("#second_quiz");
