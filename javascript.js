7e011e4974d1971a1e6c12952842605a1a485cad
Sean510 <swhite51096@gmail.com>
2021-04-14 19:29:42 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,10 +3,34 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#page_view").onclick = (e) => {
     
     handle_app_page_event(e);
+    let user_name = document.querySelector('#user_name').value;
+    
     
   };
   
 });
+
+const display_quiz = async (quiz_num, question_num) => {
+  
+  if (quiz_num == 1) {
+    
+    const data = await fetch("https://my-json-server.typicode.com/sean510/cus1172-project-3/quiz1")
+    const model = await data.json()
+    const html_element = display_question(model.question_num,model.type)
+    document.querySelector("page_view").innerHTML = html_element;
+
+
+  } else if (quiz_num == 2) {
+
+    const data = await fetch("https://my-json-server.typicode.com/sean510/cus1172-project-3/quiz2")
+    const model = await data.json()
+    const html_element = display_question(model.question_num,model.type)
+    document.querySelector("page_view").innerHTML = html_element;
+
+  }
+}
+
+
 
 function handle_app_page_event(e) {
   
@@ -14,13 +38,21 @@ function handle_app_page_event(e) {
   
   if (e.target.value == "quiz1") {
   
-    alert("quiz 1 pressed");
-    //display_quiz("#first_quiz");
+    display_quiz(1,0)
     
   } else if (e.target.value == "quiz2") {
   
-    alert("quiz 2 pressed")
-    //display_quiz("#second_quiz");
+    display_quiz(2,0);
   }
   
 };
+
+const display_question = (question_number,view) => {
+  template_source = document.querySelector(question_number).innerHTML
+
+  var template = Handlebars.compile(template_source);
+
+  var html_widget_element = template(model)
+
+  return html_widget_element
+}
